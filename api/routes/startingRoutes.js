1bc29e87b0897c684bd9f5b97091894f93fcb677
daviematz <daviematz@gmail.com>
2019-03-05 21:31:41 -0800

    added starting routes

---
+++ api/routes/startingRoutes.js
@@ -0,0 +1,14 @@
+module.exports = function(app) {
+    var testingController = require('../controllers/startingController');
+  
+    // todoList Routes
+    app.route('/tests')
+      .get(testingController.list_all_tests)
+      .post(testingController.create_a_test);
+
+    app.route('/tests/:testId')
+    .get(testingController.read_a_test)
+    .put(testingController.update_a_test)
+    .delete(testingController.delete_a_test);
+  };
+  
