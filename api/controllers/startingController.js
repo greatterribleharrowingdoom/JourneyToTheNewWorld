50a013361936743617af0cedd0e6aef2e076437b
daviematz <daviematz@gmail.com>
2019-03-05 21:36:18 -0800

    Set up the controller

--- api/controllers/startingController.js
+++ api/controllers/startingController.js
@@ -1,1 +1,53 @@
-//ab
+var mongoose = require('mongoose'),
+  Test = mongoose.model('Tests');
+
+exports.list_all_tests = function(req, res) {
+  test.find({}, function(err, test) {
+    if (err)
+      res.send(err);
+    res.json(test);
+  });
+};
+
+
+
+
+exports.create_a_test = function(req, res) {
+  var new_test = new test(req.body);
+  new_test.save(function(err, test) {
+    if (err)
+      res.send(err);
+    res.json(test);
+  });
+};
+
+
+exports.read_a_test = function(req, res) {
+  test.findById(req.params.testId, function(err, test) {
+    if (err)
+      res.send(err);
+    res.json(test);
+  });
+};
+
+
+exports.update_a_test = function(req, res) {
+  test.findOneAndUpdate({_id: req.params.testId}, req.body, {new: true}, function(err, test) {
+    if (err)
+      res.send(err);
+    res.json(test);
+  });
+};
+
+
+exports.delete_a_test = function(req, res) {
+
+
+  test.remove({
+    _id: req.params.testId
+  }, function(err, test) {
+    if (err)
+      res.send(err);
+    res.json({ message: 'test successfully deleted' });
+  });
+};
